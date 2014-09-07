3b814fc5077454bfc1b7d539b15c60cbc5db46ab
Daniel Furze <daniel@furzeface.com>
2014-09-07 17:38:51 +0100

    feat(Blog): Add method to append GitHub Gists after page ready

--- src/assets/scripts/modules/combine/global.js
+++ src/assets/scripts/modules/combine/global.js
@@ -1,22 +1,74 @@
-var bb = bb ? bb : {};
-(function ($) {
+/**
+ * @file Global module
+ * @version 1.0.0
+ * @author {@link http://github.com/furzeface Daniel Furze}
+ */
+ var bb = bb ? bb : {};
+ (function ($) {
   $.extend(bb, {
-    global: {
+    /**
+     * Site global related methods.
+     * @namespace Global
+     */
+     global: {
       bb: null,
       setGlobal: function (bb) {
         var self = this;
         self.bb = bb;
       },
+      /**
+      * Initialises Global module.
+      * @function init
+      * @memberof Global
+      */
       init : function () {
         var self = this;
 
         self.bindEvents();
+        self.appendGist();
       },
+      /**
+      * Binds global module events
+      * @function bindEvents
+      * @memberof Global
+      */
       bindEvents: function () {
-        // var self = this;
+        var self = this;
+        self.$emoji = $('.emoji');
 
-        $('.emoji').each(function(i, d){
+        self.$emoji.each(function(i, d){
           $(d).emoji();
+        });
+      },
+      /**
+      * Appends GitHub gists after pageReady
+      * @function appendGist
+      * @memberof Global
+      * @see {@link https://www.chrispoulter.com/blog/post/loading-gists-asynchronously}
+      */
+      appendGist: function () {
+        $('[data-gist]').each(function () {
+          var $element = $(this),
+          id = $element.attr('data-gist');
+
+          $element.html('<p>Loading Gist...</p>');
+
+          $.ajax({
+            url: 'https://gist.github.com/' + id + '.json',
+            dataType: 'jsonp',
+            cache: true,
+            success: function (data) {
+              if (data && data.div) {
+                if (!$('link[href="' + data.stylesheet + '"]').length) {
+                  $('head').append('<link rel="stylesheet" href="' + data.stylesheet + '"' + ' />');
+                }
+                $element.html(data.div);
+              }
+            },
+            error: function () {
+              $element.html('<p>Gist Load Failed</p>');
+            }
+          });
         });
       }
     }
