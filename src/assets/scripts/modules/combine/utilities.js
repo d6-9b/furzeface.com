c7acd802756e1c3a5ebd55e51277cb24455890a7
Daniel Furze <daniel@furzeface.com>
2014-09-08 20:05:24 +0100

    feat(JS): Namespaces Utiltiies module and adds JSdoc comments

--- src/assets/scripts/modules/combine/utilities.js
+++ src/assets/scripts/modules/combine/utilities.js
@@ -1,43 +1,93 @@
+/**
+ * @file Utilities module
+ * @version 1.0.0
+ * @author {@link http://github.com/furzeface Daniel Furze}
+ */
 var bb = bb ? bb : {};
 (function ($) {
 	$.extend(bb, {
-		getUrlParams: function (queryString) {
-			var params = {};
-			if (queryString) {
-				var queryStringArray = queryString.split('&');
-				for (var index in queryStringArray) {
-					var query = queryStringArray[index].split('=');
-					params[decodeURIComponent(query[0])] = decodeURIComponent(query[1]);
-				}
-			}
-			return params;
-		},
-		setUrlParams: function () {
-			var self = this;
-			self.settings.urlParams = self.getUrlParams(window.location.search);
-		},
-		// use for debugging/logging
-		log: function (content) {
-			if (typeof(console) !== 'undefined') {
-				console.log(content);
-			}
-		},
-		htmlEncode: function (value) {
-			if (value) {
-				return $('<div />').text(value).html();
-			} else {
-				return '';
-			}
-		},
-		htmlDecode: function (value) {
-			if (value) {
-				return $('<div />').html(value).text();
-			} else {
-				return '';
-			}
-		}
+    /**
+    * Utility methods.
+    * @namespace Utilities
+    */
+    utilities: {
+      bb: null,
+      setGlobal: function () {
+        var self = this;
+
+        self.bb = bb;
+      },
+      /**
+      * Gets parameters from a query string.
+      * @function getUrlParams
+      * @memberof Utilities
+      * @param {string} queryString
+      * @returns {object} params
+      */
+      getUrlParams: function (queryString) {
+        var params = {};
+
+        if (queryString) {
+          var queryStringArray = queryString.split('&');
+          for (var index in queryStringArray) {
+            var query = queryStringArray[index].split('=');
+            params[decodeURIComponent(query[0])] = decodeURIComponent(query[1]);
+          }
+        }
+        return params;
+      },
+      /**
+      * Sets parameters in a query string.
+      * @function setUrlParams
+      * @memberof Utilities
+      */
+      setUrlParams: function () {
+        var self = this;
+
+        self.bb.settings.urlParams = self.getUrlParams(window.location.search);
+      },
+      /**
+      * Safer and enhanced console.log()
+      * @function log
+      * @memberof Utilities
+      * @param {string} content
+      */
+      log: function (content) {
+        if (typeof(console) !== 'undefined') {
+          console.log(content);
+        }
+      },
+      /**
+      * Encodes an HTML string
+      * @function htmlEncode
+      * @memberof Utilities
+      * @param {string} value
+      * @returns {string}
+      */
+      htmlEncode: function (value) {
+        if (value) {
+          return $('<div />').text(value).html();
+        } else {
+          return '';
+        }
+      },
+      /**
+      * Decodes an HTML string
+      * @function htmlDecode
+      * @memberof Utilities
+      * @param {string} value
+      * @returns {string}
+      */
+      htmlDecode: function (value) {
+        if (value) {
+          return $('<div />').html(value).text();
+        } else {
+          return '';
+        }
+      }
+    }
 	});
-	$.subscribe('pageReady', function () {
-		bb.setUrlParams();
-	});
+  $.subscribe('setGlobal', function (event, bb) {
+    bb.utilities.setGlobal(bb);
+  });
 }(jQuery));
