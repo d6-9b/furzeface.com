470f1cca5b956645d635e8d18c4a5a6d3329015f
Daniel Furze <daniel@furzeface.com>
2014-09-08 12:44:33 +0100

    feat(JSDoc): Adds JSDoc comments to helpers

--- src/helpers/helper-if_block.js
+++ src/helpers/helper-if_block.js
@@ -1,15 +1,25 @@
-module.exports.register = function (Handlebars, options, params)  {
-	Handlebars.registerHelper('if_block', function (names, options) {
-		this.blocks = this.blocks || {};
-		names = names.split('|');
-		for(var i=0; i < names.length; i++) {
-			var name = names[i].trim(),
-				block = this.blocks[name];
-			if (block) {
-				return options.fn(this);
-			} else {
-				return options.inverse(this);
-			}
-		}
-	});
+/**
+ * @file Returns contents of block if they exist.
+ * @version 1.0.0
+ * @author {@link http://github.com/spacedawwwg Paul Welsh}
+ */
+
+ module.exports.register = function (Handlebars, options, params)  {
+  /**
+  * Handlebars helpers.
+  * @namespace Handlebars.helpers
+  */
+  Handlebars.registerHelper('if_block', function (names, options) {
+    this.blocks = this.blocks || {};
+    names = names.split('|');
+    for(var i=0; i < names.length; i++) {
+      var name = names[i].trim(),
+      block = this.blocks[name];
+      if (block) {
+        return options.fn(this);
+      } else {
+        return options.inverse(this);
+      }
+    }
+  });
 };
