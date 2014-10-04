b53851702d4aa0d823b0f2b015927eac7d128975
Daniel Furze <daniel@furzeface.com>
2014-10-04 20:51:45 +0100

    feat(Text resizer): Adds method for text enlargement and button, unpublished :heavy_plus_sign:

--- src/assets/scripts/modules/combine/global.js
+++ src/assets/scripts/modules/combine/global.js
@@ -15,6 +15,8 @@
       $images: $('img'),
       $gists: $('[data-gist]'),
       $emoji:  $('.emoji'),
+      originalFontSize: parseFloat(ff.settings.$html.css('font-size')),
+      resizeCount: 0,
       setGlobal: function (ff) {
         var self = this;
 
@@ -28,10 +30,28 @@
       init: function () {
         var self = this;
 
+        self.bindEvents();
+
         self.appendGists();
         self.setEmojis();
         // self.syntaxHighlight();
         self.lazyImages();
+      },
+      /**
+      * Binds Global module events.
+      * @function bindEvents
+      * @memberof Global
+      */
+      bindEvents: function () {
+        var self = this;
+
+        self.$textResizer = $('#text_resizer');
+
+        self.$textResizer.on('click', function (e) {
+          e.preventDefault();
+
+          self.enlargeText();
+        });
       },
       /**
       * Appends GitHub gists after pageReady.
@@ -98,13 +118,35 @@
         var self = this;
 
         self.$images.unveil(400);
+      },
+      /**
+      * Text enlargement.
+      * @function enlargeText
+      * @memberof Global
+      */
+      enlargeText: function () {
+        var self = this;
+
+        if (self.resizeCount < 6) {
+          // Updates counter
+          self.resizeCount = self.resizeCount + 1;
+          // Calculates new font size
+          var fontSize = parseFloat(self.ff.settings.$html.css('font-size')) * 1.1;
+          // Sets new font size
+          self.ff.settings.$html.css('font-size', fontSize);
+        } else {
+          // Else reset font size
+          self.ff.settings.$html.css('font-size', self.originalFontSize);
+          // And reset counter
+          self.resizeCount = 1;
+        }
       }
     }
   });
-  $.subscribe('setGlobal', function (event, ff) {
-    ff.global.setGlobal(ff);
-  });
-  $.subscribe('pageReady', function () {
-    ff.global.init();
-  });
+$.subscribe('setGlobal', function (event, ff) {
+  ff.global.setGlobal(ff);
+});
+$.subscribe('pageReady', function () {
+  ff.global.init();
+});
 }(jQuery));
